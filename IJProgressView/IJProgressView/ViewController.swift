f8c874355482032207a17c032a7dc5eb4f4af56b
Isuru Nanayakkara <contact.isuru@gmail.com>
2015-01-14 22:33:11 +0530

    Initial commit of the renamed project.

---
+++ IJProgressView/IJProgressView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IJProgressView
+//
+//  Created by Isuru Nanayakkara on 1/14/15.
+//  Copyright (c) 2015 Appex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
