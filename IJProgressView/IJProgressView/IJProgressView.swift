5625f35548f2ade91d03eb5257c5ed29e1b06f1d
PHUNG <phung.duy@newunion.sg>
2018-07-05 13:53:15 +0700

    Fix convert navigationbar, tabbar

--- IJProgressView/IJProgressView/IJProgressView.swift
+++ IJProgressView/IJProgressView/IJProgressView.swift
@@ -42,6 +42,49 @@ open class IJProgressView {
         
         activityIndicator.startAnimating()
     }
+    open func showProgressView() {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        containerView.frame = window.frame
+        
+        containerView.center = window.center
+        
+        containerView.backgroundColor = UIColor(hex: 0xffffff, alpha: 0.9)
+        
+        
+        
+        progressView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        
+        progressView.center = window.center
+        
+        progressView.backgroundColor = UIColor(hex: 0x444444, alpha: 0.7)
+        
+        progressView.clipsToBounds = true
+        
+        progressView.layer.cornerRadius = 10
+        
+        
+        
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        
+        activityIndicator.activityIndicatorViewStyle = .whiteLarge
+        
+        activityIndicator.center = CGPoint(x: progressView.bounds.width / 2, y: progressView.bounds.height / 2)
+        
+        
+        
+        progressView.addSubview(activityIndicator)
+        
+        containerView.addSubview(progressView)
+        
+        //view.addSubview(containerView)
+        
+        UIApplication.shared.keyWindow?.addSubview(containerView)
+        
+        activityIndicator.startAnimating()
+        
+    }
     
     open func hideProgressView() {
         activityIndicator.stopAnimating()
